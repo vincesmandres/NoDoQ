eeea4a95fa06fa2a62eeca7398d3b3aefb64d49d
dvizc <domenicavizcarra3410@gmail.com>
2025-09-20 20:43:52 -0500

    Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const maci = require('./services/maciCoordinator');
 const app = require('./app');
 const { logger } = require('./utils/logger');
 const config = require('./config');
@@ -7,4 +8,10 @@ const PORT = config.PORT || 4000;
 
 app.listen(PORT, () => {
   logger.info(`NoDo backend listening on port ${PORT}`);
+});
+
+maci.initTables().then(() => {
+  logger.info('MACI tables ready');
+}).catch(err => {
+  logger.error('Error initializing MACI tables', err);
 });
