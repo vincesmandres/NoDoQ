2d4ee1adeb88153ad8129c1007671d4302243884
vincesmandres <vincesmandres@gmail.com>
2025-09-20 19:30:27 -0500

    update error

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+import path from "path";
+const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
